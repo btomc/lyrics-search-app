7fdebc42803467c407210094bed0fdcb9f977c34
Beata Tomczak <tma.btty@gmail.com>
2020-08-27 19:54:15 +0200

    get & display song lyrics

--- script.js
+++ script.js
@@ -43,6 +43,18 @@ async function getMoreSongs(url) {
 
     showData(data)
 }
+
+// Get lyrics for song
+async function getLyrics(artist, songTitle) {
+    const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`)
+    const data = await res.json()
+
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>')
+    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+    <span>${lyrics}</span>`
+
+    more.innerHTML = ''
+}
  
 
 form.addEventListener('submit', e => {
@@ -57,3 +69,15 @@ form.addEventListener('submit', e => {
     }
     
 })
+
+// Get lyrics btn click
+result.addEventListener('click', e => {
+    const clickedEl = e.target
+
+    if (clickedEl.tagName === 'BUTTON') {
+        const artist = clickedEl.getAttribute('data-artist')
+        const songTitle = clickedEl.getAttribute('data-songtitle')
+
+        getLyrics(artist, songTitle)
+    }
+}) 
